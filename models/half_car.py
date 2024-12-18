f3c8bb12dd76d475eafe4f8f853c7691b23db893
Ahmet TARHAN <46814161+rizoit@users.noreply.github.com>
2024-12-18 14:46:20 +0100

    add delay time between front and rear tires

--- models/half_car.py
+++ models/half_car.py
@@ -34,7 +34,7 @@ class HalfCarModel(VehicleModel):
             np.ndarray: Derivatives of the state vector.
         """
         z_s, z_s_dot, theta, theta_dot, z_u_f, z_u_f_dot, z_u_r, z_u_r_dot = y
-        
+
         # Extract parameters
         ms, I = self.params['ms'], self.params['I']
         mu_f, mu_r = self.params['mu_f'], self.params['mu_r']
@@ -43,33 +43,25 @@ class HalfCarModel(VehicleModel):
         ku_f, ku_r = self.params['ku_f'], self.params['ku_r']
         a, b = self.params['a'], self.params['b']
         longitudinal_velocity = self.params['longitudial_velocity']
-        
-    
+
         x_s, x_s_dot, theta, theta_dot, x_u_f, x_u_f_dot, x_u_r, x_u_r_dot = y
-        
-        
-        # Calculate forces
-        x_g_f = u(t)
 
-        delay_between_tires = (a + b)/longitudinal_velocity
-
-        if t<delay_between_tires:
-            x_g_r = 0
-        else:
-            x_g_r = u(t-delay_between_tires)
+        # external displacements of tires
+        delay_time = (a + b) / longitudinal_velocity
+        x_g_f = u(t)  # front tire external displacement
+        x_g_r = u(max(0, t - delay_time))  # rear tire external displacement with delay
 
         # Calculate accelerations
         F_u_r = ku_r*(x_g_r - x_u_r)
         F_u_f = ku_f*(x_g_f - x_u_f)
         F_s_r = ks_r*(x_u_r - x_s + b*theta) + cs_r*(x_u_r_dot - x_s_dot + b*theta_dot)
         F_s_f = ks_f*(x_u_f - x_s - a*theta) + cs_f*(x_u_f_dot - x_s_dot - a*theta_dot)
-        
 
         # Calculate accelerations
         x_u_r_ddot = (F_u_r - F_s_r)/mu_r
         x_u_f_ddot = (F_u_f - F_s_f)/mu_f
         x_s_ddot = (F_s_r + F_s_f)/ms
         theta_ddot = (a * F_s_f - b * F_s_r) / I
-        
-        #return np.array([z_s_dot, z_s_ddot, theta_dot, theta_ddot, z_u_f_dot, z_u_f_ddot, z_u_r_dot, z_u_r_ddot])
+
         return np.array([x_s_dot, x_s_ddot, theta_dot, theta_ddot, x_u_f_dot, x_u_f_ddot, x_u_r_dot, x_u_r_ddot])
+    
